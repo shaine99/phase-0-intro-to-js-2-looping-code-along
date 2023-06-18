f01069ca57298815e2d3f372acb3e875b3150b71
Shaine <shaine99waters@gmail.com>
2023-06-18 14:30:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards) { 
+    const thankYouCards = [] 
+    for (let s = 0; s < cards.length; s++) {
+        thankYouCards.push(`Thank you, ${cards[s]}, for the wonderful surprise gift!`);
+    } 
+    return thankYouCards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "suprise")
+
+function countDown(n) { 
+    if (n < 0) {
+        console.log("positive integer. ");
+        return;''
+    } 
+    while (n >= 0) { 
+        console.log(n);
+        n--;
+    }
+
+}
+ 
+
+
